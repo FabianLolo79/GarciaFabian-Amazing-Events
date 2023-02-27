a9581860fe3bf6c16b088c379c2442f93df68fb5
Fabián Enrique García <fabianegl79@gmail.com>
2023-02-27 16:49:08 -0300

    tarea2 agregados

--- assets/js/past.js
+++ assets/js/past.js
@@ -1,48 +1,20 @@
-//console.log(data);
-
 let contenedor = document.getElementById('contenedor');
 let fragment = document.createDocumentFragment();
-const fechaActual = Date.parse(data.currentDate);
-
-for (let event of data.events) { 
-        let fechaEvento = Date.parse(event.date);   
-            if (fechaEvento > fechaActual) {
-                let div = document.createElement('div')
-                div.classList.add("card")
-                div.style.width = "18rem"
-                div.innerHTML = `<img src=${event.image} class="card-img-top " alt="...">
-                                <div class="card-body text_center">
-                                    <h5 class="card-title title_card">${event.name}</h5>
-                                    <p class="card-text">${event.description}</p>
-                                    <p>Price: ${event.price} U$D</p>
-                                    <a href="./description.html" class="btn btn-outline-danger shadow">Go somewhere</a>
-                                </div>`            
-            }             
-}
-
-console.log(fechaActual);
-
-
-/* 
-let fragment = document.createDocumentFragment()
 
 for (let element of data.events) {
-
-let fechasFuturas = Date.parse(element.date)
-
-    if (fechasFuturas > fechaActual) {
-
-        let div = document.createElement('div')
-        div.classList.add("card")
-        div.style.width = "18rem"
-        div.innerHTML = <img src=${element.image} class="card-img-top " alt="...">
-                 <div class="card-body text_center">
-                     <h5 class="card-title title_card">${element.name}</h5>
-                    <p class="card-text">${element.description}</p>
-                    <p>Price: ${element.price} U$D</p>
-                     <a href="./details.html" class="navega1">View More</a>
-                 </div>
-        fragment.appendChild(div)
-    }
+    let div = document.createElement('div');
+    div.classList.add = ('card');
+    div.style.height = "100%";
+    div.innerHTML = 
+        `<img src="${element.image}" class="card-img-top object-fit-cover h-100 image" alt="...">
+    <div class="card-body">
+        <h5 class="card-title text-center titulo">${element.name}</h5>
+        <p class="card-text text-center">${element.category}</p>
+        <div class="d-flex justify-content-between mt-5 mb-0">
+            <p>${element.price}</p>
+            <a href="./description.html" class="btn btn-outline-danger shadow">Go somewhere</a>
+        </div>
+    </div>`
+    fragment.appendChild(div);
 }
-card_up.appendChild(fragment) */
+contenedor.appendChild(fragment);
---
+++ assets/js/upcoming.js
@@ -0,0 +1,20 @@
+let contenedor = document.getElementById('contenedor');
+let fragment = document.createDocumentFragment();
+
+for (let element of data.events) {
+    let div = document.createElement('div');
+    div.classList.add = ('card');
+    div.style.height = "100%";
+    div.innerHTML = 
+        `<img src="${element.image}" class="card-img-top object-fit-cover h-100 image" alt="...">
+         <div class="card-body">
+        <h5 class="card-title text-center titulo">${element.name}</h5>
+        <p class="card-text text-center">${element.category}</p>
+        <div class="d-flex justify-content-between mt-5 mb-0">
+            <p>${element.price}</p>
+            <a href="./description.html" class="btn btn-outline-danger shadow">Go somewhere</a>
+        </div>
+    </div>`
+    fragment.appendChild(div);
+}
+contenedor.appendChild(fragment);
