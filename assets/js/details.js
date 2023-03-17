f3bae3a8c82b13304d5cd0cc6135ec56146fd51d
Fabián Enrique García <fabianegl79@gmail.com>
2023-03-17 13:34:07 -0300

    finalizado tarea4

--- assets/js/details.js
+++ assets/js/details.js
@@ -2,59 +2,41 @@
 
 let urlApi = 'https://mindhub-xj03.onrender.com/api/amazing'
 
-async function bringData() {
+async function callDetails() {
     try{
-        // traigo todo de la API
         const response = await fetch(urlApi)
         const data = await response.json()
         const dataEvents = await data.events
-        console.log(dataEvents);
-
         const queryString = location.search;
-        
         const params = new URLSearchParams(queryString);
-        
         const id = params.get("_id");
+        const events = data.events.find(event => event._id == id);
 
-        const happening = data.events.find(happen => happen._id == id);
-
-        details(happening, "eventContainer")
+        details(events, "eventContainer")
     } catch(error){
         console.log(error);
     }
 }
-bringData();
+callDetails();
 
-// const queryString = location.search;
-
-// const params = new URLSearchParams(queryString);
-
-// const id = params.get("_id");
-
-// const happening = data.events.find(happen => happen._id == id);
-
-
-// ORIGINAL DEL DETAILS
 function details(array, idContainer) {
     const div = document.getElementById(idContainer)
-    div.innerHTML = `<div class="img-details">
-                    <img class="card-img-top h-50" src="${array.image}">
-                    <div class="card-body d-flex flex-column details-div">
-                    <h5 class="h2-details">${array.name}</h5>
-                    <p class="p-details"> Description: ${array.description} </p>
-                    <ul class="ul-details">
-                        <li class="ul-details">Date: ${array.date}</li>
-                        <li class="ul-details">Category: ${array.category}</li>
-                        <li class="ul-details">Place: ${array.place}</li>
-                        <li class="ul-details">Capacity: ${array.capacity} people</li>
-                        <li class="ul-details">Assistance: ${array.estimate}</li>
-                        <li class="ul-details">Price: ${array.price} USD</li>
-                    </ul>
-                    </div>
-                    </div>`
+    div.innerHTML = `<div class="row">
+    <div class="col-md-6">
+        <img src="${array.image}" alt="..." class="mb-4 mb-md-0 pt-5" style="width: 600px; height: 500px;"/>
+    </div>
+    <div class="col-md-6 d-flex flex-column justify-content-center pt-3">
+        <h2 class="details_title">${array.name}</h2>
+        <p>${array.description}</p>
+        <p class="h6"><strong>Date: </strong>${array.date}</p>
+        <p><strong>Category: </strong>${array.category}</p>
+        <p><strong>Place: </strong>${array.place}</p>
+        <p><strong>Capacity: </strong>${array.capacity}</p>
+        <p><strong>Assistance: </strong>${array.assistance}</p>
+        <p><strong>Price: </strong>$${array.price}</p>
+        </div>
+    </div>`
 }
-
-// details(happening, "details-card")
 
 
 
--- assets/js/past.js
+++ assets/js/past.js
@@ -1,70 +1,102 @@
+let urlApi = "https://mindhub-xj03.onrender.com/api/amazing ";
+
+async function getData() {
+    try {
+        let response = await fetch(urlApi);
+        let data = await response.json();
+        let dataEvents = data.events;
+        //console.log(dataEvents);
+        //console.log(data);
+
+        showCards(dataEvents, 'containerHome');
+        createChecks(dataEvents);
+        filterAll(dataEvents);
+
+        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(check => check.addEventListener("change", () => {
+            selectChecked = Array.from(checkboxes).filter(check => check.checked)
+                .map(elem => elem.value)
+            //console.log("Hola check");
+            filterAll(dataEvents);
+        }))
+
+        let inputSearch = document.getElementById('input-search');
+        inputSearch.addEventListener('keyup', (e) => {
+            inputText = inputSearch.value;
+            filterAll(dataEvents);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+getData();
+
 /* ---------- Mostrar tarjetas dinámicas ----------*/
-function showCards(array,idcontainer) {
+function showCards(array, idcontainer) {
     let container = document.getElementById(idcontainer);
     let fragment = document.createDocumentFragment();
-    const diaActual = new Date (data.currentDate);
+    //console.log(array);
     container.innerHTML = '';
     if (array.length == 0) {
         let div = document.createElement("div");
-        div.innerHTML=`<p class="text-center p-1 shadow">No se encontró el evento buscado</p>`
+        div.innerHTML = `<p class="text-center p-1 shadow">Search not found</p>`
         container.appendChild(div);
     } else {
-        for (let element of data.events) {
-            let diaEvento = new Date(element.date);
-
-                if(diaActual > diaEvento) {
-                    let div = document.createElement('div');
-                    div.classList.add('card', 'ms-5', 'shadow');
-                    div.style.width = "20rem";
-                    div.innerHTML = 
-                        `<img src="${element.image}" class="card-img-top object-fit-cover mt-3 image" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-center titulo">${element.name}</h5>
-                            <p class="card-text text-center">${element.description}</p>
-                            <p class="card-text text-center fw-bold">Date: ${element.date}</p>
-                            <div class="d-flex justify-content-between mt-5 mb-0">
-                                <p class="fw-bold">Price: $${element.price}</p>
-                                <a href="./details.html?_id=${element._id}" class="btn btn-outline-danger shadow">Details</a>
-                            </div>
-                        </div>`
-                    fragment.appendChild(div);
-                    }
+        //console.log(array);
+        for (let element of array) {
+            let div = document.createElement('div');
+            div.className = 'card ms-5 shadow';
+            div.style.width = '20rem';
+            div.innerHTML =
+                ` 
+                    <img src="${element.image}" class="card-img-top object-fit-cover mt-3 image" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center titulo">${element.name}</h5>
+                        <p class="card-text text-center">${element.description}</p>
+                        <p class="card-text text-center fw-bold">Date: ${element.date}</p>
+                        <div class="d-flex justify-content-between mt-5 mb-0">
+                            <p class="fw-bold">Price: $${element.price}</p>
+                            <a href="./details.html?_id=${element._id}" class="btn btn-outline-danger shadow">Details</a>
+                        </div>
+                    </div>
+                `
+            fragment.appendChild(div);
         }
         container.appendChild(fragment);
     }
 }
-showCards(data.events,'containerPast');
+//showCards(data.events, 'containerHome');
 
 /* ---------- Checks Category dinamic ----------*/
 function createChecks(array) {
-    let containerCheckPast = document.getElementById('containerCheckPast');
-    let fragmentCheckPast = document.createDocumentFragment();
+    let containerCheckHome = document.getElementById('containerCheckHome');
+    let fragmentCheck = document.createDocumentFragment();
     let checkCategory = [...(new Set(array.map(data => data.category)))];
     checkCategory.forEach(category => {
         let form = document.createElement('form');
         form.classList.add('d-flex', 'py-5');
-        form.innerHTML = 
-        `
+        form.innerHTML =
+            `
             <div class="col-auto mx-5">
                 <div class="form-check category1 category" id="form-category"> 
                     <label class="form-check-label">
-                        ${category.replace(" ","-")} <input class="form-check-input mx-1 shadow prueba" type="checkbox" value=${category.replace(" ","")}> 
+                        ${category.replace(" ", "-")} <input class="form-check-input mx-1 shadow prueba" type="checkbox" value=${category.replace(" ", "")}> 
                     </label>
                 </div> 
             </div>    
         `
-        fragmentCheckPast.appendChild(form);
+        fragmentCheck.appendChild(form);
     })
-    containerCheckPast.appendChild(fragmentCheckPast);
+    containerCheckHome.appendChild(fragmentCheck);
 }
-createChecks(data.events);
+//createChecks(data.events);
 
 /* ---------- Filter cruzados ----------*/
-let selectCheckedPast = [];
-let inputTextPast = '';
+let selectChecked = [];
+let inputText = '';
 
 function filterArrayToArray(arrayStrings, arrayObject) {
-    return arrayStrings.length === 0 ? arrayObject : arrayObject.filter(elemento => arrayStrings.includes(elemento.category.replace(" ","")))
+    return arrayStrings.length === 0 ? arrayObject : arrayObject.filter(elemento => arrayStrings.includes(elemento.category.replace(" ", "")))
 }
 
 function filterArrayToString(value, arrayObject) {
@@ -72,25 +104,10 @@ function filterArrayToString(value, arrayObject) {
     return arrayObject.filter(object => object.name.toLowerCase().includes(value.toLowerCase().trim()))
 }
 
-let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach(check => check.addEventListener("change", () => {
-    selectCheckedPast = Array.from(checkboxes).filter(check => check.checked)
-    .map(elem => elem.value)
-    console.log("Hola check");
-    console.log(filterAll()); // undefined en la función 
-}))
-
-let inputSearchPast = document.getElementById('input-search');
-    inputSearchPast.addEventListener('keyup', (e) => {
-    inputTextPast = inputSearchPast.value;
-    console.log("Hola search");
-    console.log(filterAll());
-})
-
 /* ---------- Final Filter ----------*/
-function filterAll() {
-    let cardsChecksFilteredPast = filterArrayToArray(selectCheckedPast, data.events);
-    let cardsFinalFilteredPast = filterArrayToString(inputTextPast, cardsChecksFilteredPast);
-    showCards(cardsFinalFilteredPast, 'containerPast');
+function filterAll(array) {
+    let cardsChecksFiltered = filterArrayToArray(selectChecked, array);
+    let cardsFinalFiltered = filterArrayToString(inputText, cardsChecksFiltered);
+    showCards(cardsFinalFiltered, 'containerHome');
 }
 
